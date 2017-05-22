59829201f3ec8863b5430b4350b7387b836e6bf4
Tom <Tom Hewuse>
2017-05-22 15:34:41 -0500

    finished js fundamentals 2

--- mean/javascript/js-fundamentals2/main.js
+++ mean/javascript/js-fundamentals2/main.js
@@ -23,7 +23,6 @@ function sumUp(x,y){
 console.log(sumUp(2,5));
 
 //Write a loop that will go through an array, find the minimum value, and then return it
-//Write a loop that will go through the array [1, 5, 90, 25, -3, 0], find the minimum value, and then print it
 function arrayMin(arr){
     var min=arr[0];
     for(let i=0; i<arr.length; i++){
@@ -36,3 +35,120 @@ function arrayMin(arr){
 console.log(arrayMin([1, 5, 90, 25, -3, 0]));
 
 //Write a loop that will go through an array, find the average of all of the values, and then return it
+function arrayAvg(arr){
+    var sum=0;
+    arr.forEach((value)=>sum+=value);
+    return sum/arr.length;
+}
+var testArr = [1, 2, 3, 4];
+console.log(arrayAvg(testArr));
+
+
+//Rewrite these 3 as anonymous functions assigned to variables.
+var sumVar = function(x,y){
+    let newArr=[];
+    let start = 0;
+    let end = 0;
+    let sum = 0;
+    if(x>y){
+        start = y;
+        end = x;
+    }
+    else{
+        start=x;
+        end=y;
+    }
+    for(let i=start; i<=end; i++){
+        sum+=i;
+    }
+    return sum;
+}
+
+var minVar = function(arr){
+    var min=arr[0];
+    for(let i=0; i<arr.length; i++){
+        if(min>arr[i]){
+            min=arr[i];
+        }
+    }
+    return min;
+}
+
+var avgVar = function (arr){
+    var sum=0;
+    arr.forEach((value)=>sum+=value);
+    return sum/arr.length;
+}
+
+
+//Rewrite these as methods of an object
+var objectFuncs = {
+    sumUp: function(x,y){
+        let newArr=[];
+        let start = 0;
+        let end = 0;
+        let sum = 0;
+        if(x>y){
+            start = y;
+            end = x;
+        }
+        else{
+            start=x;
+            end=y;
+        }
+        for(let i=start; i<=end; i++){
+            sum+=i;
+        }
+        return sum;
+    },
+    getmin: function(x,y){
+        let newArr=[];
+        let start = 0;
+        let end = 0;
+        let sum = 0;
+        if(x>y){
+            start = y;
+            end = x;
+        }
+        else{
+            start=x;
+            end=y;
+        }
+        for(let i=start; i<=end; i++){
+            sum+=i;
+        }
+        return sum;
+    },
+    getAvg: function (arr){
+        var sum=0;
+        arr.forEach((value)=>sum+=value);
+        return sum/arr.length;
+    }
+}
+
+var person = {
+    name: 'Tom',
+    distance_traveled: 0,
+    say_name: function(){console.log(this.name)},
+    say_something: function(something){console.log(`${this.name} says '${something}'.`)},
+    walk: function(){
+        alert(`${this.name} is walking`);
+        this.distance_traveled+=3;
+    },
+    run: function(){
+        alert(`${this.name} is running`);
+        this.distance_traveled+=10;
+    },
+    crawl: function(){
+        alert(`${this.name} is crawling`);
+        this.distance_traveled+=1;
+    },
+
+};
+
+person.say_name();
+person.say_something('im cool');
+person.walk();
+person.run();
+person.crawl();
+console.log(person.distance_traveled);
