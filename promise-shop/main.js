7224bfbdfc708afcf11e8e6f60d9f1f92b6a861b
Tom <Tom Hewuse>
2017-05-23 15:55:58 -0500

    done with promises 4

--- promise-shop/main.js
+++ promise-shop/main.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var promise = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        reject(new Error('REJECTED!'));
-    },300);
+    resolve('I FIRED');
+    reject(new Error('I DID NOT FIRE'));
+
+    // setTimeout(function(){
+    //     reject(new Error('REJECTED!'));
+    // },300);
 });
 
 function onReject(err){
