aba040ceb0119dfee2e76659f54d064057c8abcf
Tom <Tom Hewuse>
2017-05-25 21:32:43 -0500

    finsihed survey form SPA

---
+++ mean/node/survey-form/client/js/main.js
@@ -0,0 +1,84 @@
+/**
+ * Created by Hew4803 on 5/25/17.
+ */
+'use strict';
+function renderCard(data){
+    let infoHtml = `<div id="surveyForm">
+                        <div class="input">
+                            <label for="name">Your Name:</label>
+                            <p>${data.name}</p>
+                        </div>
+                        <div class="input">
+                            <label for="location">Dojo Location:</label>
+                            <p>${data.location}</p>
+                        </div>
+                        <div class="input">
+                              <label for="language">Favorite Language: </label>
+                              <p>${data.language}</p>
+                        </div>
+                        <div id="comments">
+                              <label for="comments">Comments</label>
+                              <p>${data.comments}</p>
+                        </div>
+                        <button id="goBack">Go Back</button>
+                    </div>`;
+    $('#container').html(infoHtml);
+    $('#goBack').on('click', function(event){
+        renderInputForm();
+    });
+}
+
+function renderInputForm(){
+    let inputHtml = `<form id="surveyForm" action="/result" method="post">
+                        <div class="input">
+                            <label for="name">Your Name:</label>
+                            <input type="text" name="name">
+                        </div>
+                        <div class="input">
+                            <label for="location">Dojo Location:</label>
+                            <select name="location">
+                                <option value="Milwaukee">Milwaukee</option>
+                                <option value="Phoenix">Phoenix</option>
+                                <option value="New York">New York</option>
+                                <option value="Franklin">Franklin</option>
+                            </select>
+                        </div>
+                        <div class="input">
+                            <label for="language">Favorite Language: </label>
+                            <select name="language">
+                                <option value="Javascript">Javascript</option>
+                                <option value="C++">C++</option>
+                                <option value="Java">Java</option>
+                                <option value="PL1">PL1</option>
+                            </select>
+                        </div>
+                        <div id="comments">
+                            <label for="comments">Comments</label>
+                            <input id="commentBox" type="text" name="comments">
+                        </div>
+                        <input id="submit" type="submit" value="Submit">
+                    </form>`;
+    $('#container').html(inputHtml);
+}
+
+$(document).ready(function() {
+    renderInputForm();
+
+    $('#container').on('submit', 'form', function (event) {
+        event.preventDefault();
+        //ajax call to send data
+        let formData = $(this).serialize();
+        console.log(formData)
+        $.ajax({
+            url: '/result',
+            method: 'POST',
+            data: formData,
+            success: function(data){
+                //success receives json back with data
+                console.log(data);
+                renderCard(data);
+            }
+        });
+        //function builds card with data received
+    })
+});
--- mean/node/survey-form/server/config/routes.js
+++ mean/node/survey-form/server/config/routes.js
@@ -1,11 +1,13 @@
 module.exports=function routes(app){
 
     app.get('/',(req,res)=>{
+
         res.render('index.ejs')
     });
 
+
     app.post('/result',(req,res)=>{
-        res.render('result.ejs',{name:req.body.name,location:req.body.location,language:req.body.language,comments:req.body.comments})
+        res.json(req.body);
     });
 
 };
